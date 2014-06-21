f70e0373966fd62cfbcd43c28dbe4433f64217e0
Jeremy Kahn <jeremyckahn@gmail.com>
2014-06-21 18:37:56 -0500

    Ports auto-update-textfield from Stylie.

---
+++ src/backbone/auto-update-textfield/auto-update-textfield.js
@@ -0,0 +1,69 @@
+define([
+
+  'jquery'
+  ,'underscore'
+  ,'backbone'
+
+], function (
+
+  $
+  ,_
+  ,Backbone
+
+) {
+  'use strict';
+
+  return Backbone.View.extend({
+
+    events: {
+      'keyup': 'onKeyup'
+      ,'keydown': 'onKeydown'
+      ,'blur': 'onBlur'
+    }
+
+    ,onKeyup: function () {
+      var val = this.$el.val();
+      this.onValReenter(val);
+    }
+
+    /**
+     * @param {jQuery.Event} evt
+     */
+    ,onKeydown: function (evt) {
+      var which = +evt.which;
+
+      if (which === 38) { // up
+        this.onArrowUp(evt);
+      } else if (which === 40) { // down
+        this.onArrowDown(evt);
+      } else if (which === 13) { // enter
+        this.onEnterDown(evt);
+      } else if (which === 27) { // escape
+        this.onEscapeDown(evt);
+      }
+    }
+
+    ,tearDown: function () {
+      this.remove();
+      _.empty(this);
+    }
+
+    ,onEscapeDown: function () {
+      this.$el.trigger('blur');
+    }
+
+    // Bindable events
+
+    ,onBlur: $.noop // function(jQuery.Event)
+
+    ,onArrowUp: $.noop // function (jQuery.Event)
+
+    ,onArrowDown: $.noop // function (jQuery.Event)
+
+    ,onEnterDown: $.noop // function (jQuery.Event)
+
+    ,onValReenter: $.noop // function (jQuery.Event)
+
+  });
+
+});
